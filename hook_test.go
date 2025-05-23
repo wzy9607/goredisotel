36081e6c85dd70d191093b875108c9d4a1018e2c
Cattī Crūdēlēs <17695588+wzy9607@users.noreply.github.com>
2025-05-24 00:32:42 +0800

    feat(semconv): support db.stored_procedure.name attr for non-pipeline (#58)

--- hook_test.go
+++ hook_test.go
@@ -296,6 +296,56 @@ func Test_clientHook_ProcessHook(t *testing.T) { //nolint:maintidx //table drive
 				})
 			},
 		}, {
+			name: "default config, evalsha, success",
+			fields: fields{
+				rdsOpt: &redis.Options{Addr: "10.1.1.1:6379", DB: 3},
+			},
+			args: args{
+				hook: func(ctx context.Context, cmd redis.Cmder) error { return nil },
+				cmd:  redis.NewCmd(context.Background(), "evalsha", "sha1", 1, "key1", "key2"),
+			},
+			checkSpan: func(t *testing.T, span sdktrace.ReadOnlySpan) {
+				t.Helper()
+				assert.Equal(t, "evalsha", span.Name())
+				assert.Equal(t, sdktrace.Status{Code: codes.Unset}, span.Status())
+				t.Logf("attrs: %v", span.Attributes())
+
+				wantAttrs := []attribute.KeyValue{
+					semconv.DBSystemNameRedis,
+					semconv.DBNamespace("3"),
+					semconv.DBOperationName("evalsha"),
+					semconv.ServerAddress("10.1.1.1"),
+					semconv.ServerPort(6379),
+					semconv.DBStoredProcedureName("sha1"),
+				}
+				assert.Subset(t, span.Attributes(), wantAttrs)
+
+				wantNotExistAttrs := []attribute.Key{semconv.DBResponseStatusCodeKey, semconv.DBQueryTextKey}
+				attrs := attrMap(span.Attributes())
+				for _, key := range wantNotExistAttrs {
+					assert.NotContains(t, attrs, key)
+				}
+			},
+			checkMetrics: func(t *testing.T, sm metricdata.ScopeMetrics) {
+				t.Helper()
+				require.Len(t, sm.Metrics, 2)
+
+				assertOprDuration(t, sm.Metrics[0], []attribute.KeyValue{
+					semconv.DBSystemNameRedis,
+					semconv.DBNamespace("3"),
+					semconv.DBOperationName("evalsha"),
+					semconv.ServerAddress("10.1.1.1"),
+					semconv.ServerPort(6379),
+					semconv.DBStoredProcedureName("sha1"),
+				})
+
+				assertUseTime(t, sm.Metrics[1], []attribute.KeyValue{
+					semconv.DBSystemNameRedis,
+					semconv.DBClientConnectionPoolName("10.1.1.1:6379/3"),
+					attribute.String("status", "ok"),
+				})
+			},
+		}, {
 			name: "default config, return nil",
 			fields: fields{
 				rdsOpt: &redis.Options{Addr: "10.1.1.1:6379", DB: 3},
